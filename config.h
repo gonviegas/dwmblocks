4e3e6d0ab0503c38624b9f2d734ad70737c18fac
gonviegas <gonv@tuta.io>
2023-02-10 15:19:34 +0000

    config.h update

--- config.h
+++ config.h
@@ -1,16 +1,18 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-/*Icon*/	   /*Command*/      /*Update Interval*/  /*Update Signal*/
+/*Icon*/	   /*Command*/		        /*Update Interval*/  /*Update Signal*/
   {"", "dwmb-camera",                 3,              10},
   {"", "dwmb-mic",                    3,              7},
   {"", "dwmb-nettraf",                1,              25},
   {"", "dwmb-disks",                  60,             16},
   {"", "dwmb-memory",                 3,              13},
-  {"", "dwmb-cpu-gpu",                3,              19},
+  // {"", "dwmb-cpu-gpu",                3,              19},
+  {"", "dwmb-cpu-gpu.laptop",                3,              19},
   {"", "dwmb-volume",                 0,              5},
-  {"", "dwmb-monitor",                0,              6},
+  // {"", "dwmb-monitor",                0,              6},
+  {"", "dwmb-monitor.laptop",                0,              6},
   {"", "dwmb-updates",                900,            9},
- // {"", "dwmb-power",                  60,             8},
+  {"", "dwmb-power.laptop",                  60,             8},
   {"", "dwmb-datetime",               60,             4},
   {"", "dwmb-kblayout",               0,              1},
 };
